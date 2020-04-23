2a91c13a67c5bf41ff74ecfe2fa872248b701462
Marcel Matthijs <marcel@tradecloud.nl>
2020-04-23 20:58:59 +0200

    [TC-5873] Add Authentication Refresh and Logout

--- authentication/Program.cs
+++ authentication/Program.cs
@@ -12,9 +12,9 @@ namespace Com.Tradecloud1.SDK.Client
         // https://swagger-ui.accp.tradecloud1.com/?url=https://api.accp.tradecloud1.com/v2/authentication/specs.yaml#/authentication/login
         const string authenticationUrl = "https://api.accp.tradecloud1.com/v2/authentication/login";
         // Fill in mandatory username
-        const string username = "frankjan@tradecloud1.com";
+        const string username = "";
         // Fill in mandatory password
-        const string password = "SecretSecret1";
+        const string password = "";
 
         static async Task Main(string[] args)
         {
@@ -22,8 +22,26 @@ namespace Com.Tradecloud1.SDK.Client
             
             HttpClient httpClient = new HttpClient();
             var authenticationClient = new Authentication(httpClient, authenticationUrl);
-            var token = await authenticationClient.Authenticate(username, password);
-            Console.WriteLine("Token: " + token);
+            
+            Console.WriteLine("Authenticating..");
+            var (accessToken, refreshToken) = await authenticationClient.Authenticate(username, password);
+            Console.WriteLine("accessToken: " + accessToken);
+            Console.WriteLine("refreshToken: " + refreshToken);
+
+            Console.WriteLine("Refreshing...");
+            var (refreshedAccessToken, refreshedRefreshToken) = await authenticationClient.Refresh(refreshToken);
+            Console.WriteLine("refreshedAccessToken: " + refreshedAccessToken);
+            Console.WriteLine("refreshedRefreshToken: " + refreshedRefreshToken);
+
+            Console.WriteLine("Logout...");
+            var (loggedOutAccessToken, loggedOutRefreshToken) = await authenticationClient.Logout(refreshedAccessToken);
+            Console.WriteLine("loggedOutAccessToken: " + loggedOutAccessToken);
+            Console.WriteLine("loggedOutRefreshToken: " + loggedOutRefreshToken);
+
+            Console.WriteLine("Refreshing after logout...");
+            var (refreshedAfterLogoutAccessToken, refreshedAfterLogoutRefreshToken) = await authenticationClient.Refresh(refreshToken);
+            Console.WriteLine("refreshedAfterLogoutAccessToken: " + refreshedAfterLogoutAccessToken);
+            Console.WriteLine("refreshedAfterLogoutRefreshToken: " + refreshedAfterLogoutRefreshToken);
         }
     }
 }
