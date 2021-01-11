d161c1066c323f2734af88676a7af055de7c76db
Marcel Matthijs <marcel@tradecloud.nl>
2021-01-11 18:19:20 +0100

    [TC-6683] Update FindUserById/Email

---
+++ user-search/FindUserById/FindUserById.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Com.Tradecloud1.SDK.Client
+{
+    class FindUserById
+    {   
+        // https://swagger-ui.accp.tradecloud1.com/?url=https://api.accp.tradecloud1.com/v2/authentication/specs.yaml#/authentication/login
+        const string authenticationUrl = "https://api.accp.tradecloud1.com/v2/authentication/";
+        // Fill in mandatory username
+        const string username = "";
+        // Fill in mandatory password
+        const string password = "";
+
+        // https://swagger-ui.accp.tradecloud1.com/?url=https://api.accp.tradecloud1.com/v2/user-search/specs.yaml#/user-search/findUserByIdRoute
+        // Fill in manadatory user id
+        const string findUserByIdUrl = "https://api.accp.tradecloud1.com/v2/user-search/<id>";
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Tradecloud find user by id example.");
+            
+            HttpClient httpClient = new HttpClient();
+            var authenticationClient = new Authentication(httpClient, authenticationUrl);
+            var (accessToken, refreshToken)  = await authenticationClient.Login(username, password);
+            await findUserByIdRequest(accessToken);
+
+            async Task findUserByIdRequest(string accessToken)
+            {                
+                httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
+                var response = await httpClient.GetAsync(findUserByIdUrl);
+
+                Console.WriteLine("FindUserById StatusCode: " + (int)response.StatusCode);
+
+                string responseString = await response.Content.ReadAsStringAsync();
+                Console.WriteLine("FindUserById Content: " +  JValue.Parse(responseString).ToString(Formatting.Indented));   
+            }
+        }
+    }
+}
---
+++ user/FindUserById/README.md
@@ -0,0 +1,50 @@
+# Find user based on id
+
+This example finds a user based on id in the user service (the transaction service)
+
+## Configure
+
+In the source code:
+
+- username on Tradecloud
+- password on Tradecloud
+- user email
+
+## Run
+
+``` json
+➜  FindUserById git:(master) ✗ dotnet run
+Tradecloud find user by email example.
+Login response StatusCode: 200 ElapsedMilliseconds: 944
+Login response Content: ...
+FindUserById StatusCode: 200
+FindUserById Content: {
+  "id": "...",
+  "email": "...",
+  "roles": [
+    "buyer"
+  ],
+  "companyId": "...",
+  "status": "active",
+  "profile": {
+    "firstName": "...",
+    "lastName": "...",
+    "position": "",
+    "phoneNumber": "",
+    "linkedInProfile": ""
+  },
+  "settings": {
+    "notificationInterval": "tenminutes"
+  },
+  "createdAt": "2020-09-08T15:24:51Z",
+  "meta": {
+    "messageId": "d24ae21e-dcbf-4b45-8d20-d1400481471d",
+    "source": {
+      "traceId": "80f63ed1-767a-438e-a0a0-d83fbf745a52",
+      "userId": "...",
+      "companyId": "..."
+    },
+    "createdDateTime": "2020-09-09T08:43:52Z"
+  }
+}
+```
