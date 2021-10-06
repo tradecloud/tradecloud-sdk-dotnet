6d2eb530fa886d96478d61db4da11fad2c5cb483
marcmatt <marcel@tradecloud.nl>
2021-10-06 18:27:21 +0200

    [TC-7245] Add FindAccountByCode

---
+++ company/FindAccountByCode.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Com.Tradecloud1.SDK.Client
+{
+    class FindAccountByCode
+    {   
+         // https://swagger-ui.accp.tradecloud1.com/?url=https://api.accp.tradecloud1.com/v2/authentication/specs.yaml#/authentication/login
+        const string authenticationUrl = "https://api.accp.tradecloud1.com/v2/authentication/";
+
+        // Fill in mandatory username
+        const string username = "";
+        // Fill in mandatory password
+        const string password = "";
+
+        // https://swagger-ui.accp.tradecloud1.com/?url=https://api.accp.tradecloud1.com/v2/company/internal/specs.yaml#/company/findAccountByCodeRoute
+        const string findAccountByCodeUrl = "https://api.accp.tradecloud1.com/v2/company/{companyId}/account/{accountCode}";              
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Tradecloud find account by code example.");
+            
+            HttpClient httpClient = new HttpClient();
+            var authenticationClient = new Authentication(httpClient, authenticationUrl);
+            var (accessToken, refreshToken) = await authenticationClient.Login(username, password);
+            httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);            
+
+            await FindAccountByCode();
+
+            async Task FindAccountByCode()
+            {                
+                var start = DateTime.Now;
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                var response = await httpClient.GetAsync(findAccountByCodeUrl);
+                watch.Stop();
+
+                var statusCode = (int)response.StatusCode;
+                Console.WriteLine("FindAccountByCode start=" + start +  " elapsed=" + watch.ElapsedMilliseconds + "ms status=" + statusCode + " reason=" + response.ReasonPhrase);
+                string responseString = await response.Content.ReadAsStringAsync();
+                if (statusCode == 200)
+                    Console.WriteLine("FindAccountByCode response body=" +  JValue.Parse(responseString).ToString(Formatting.Indented));
+                else
+                    Console.WriteLine("FindAccountByCode response body=" +  responseString);
+            }
+        }
+    }
+}
