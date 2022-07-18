76c81ad030d89dccc3eba76ffbf109cc275fedcc
marcmatt <marcel@tradecloud.nl>
2022-07-18 23:22:46 +0200

    [TC-8140] Add bulk resend orders using API v1

---
+++ api-v1/ResendOrders/README.md
@@ -0,0 +1,24 @@
+# API v1 Resend Orders
+
+This code bulk resends orders to your ERP using the API v1
+
+## Prerequisites
+
+A tradecloud.nl user
+
+## Configure
+
+In the source code:
+- fill in your Tradecloud username
+- fill in your password
+- provide orders.txt with an order code on each line
+
+## Run
+
+```
+➜  api-v1 git:(master) ✗ cd ResendOrders 
+➜  ResendOrders git:(master) ✗ dotnet run
+Tradecloud resend orders.
+resendOrder code=order_code1 id=order-id-1 status=200 reason=OK
+  ...
+```
---
+++ api-v1/ResendOrders/ResendOrders.cs
@@ -0,0 +1,71 @@
+using System;
+using System.IO;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Com.Tradecloud1.SDK.Client
+{
+    class ResendOrders
+    {   
+        // Fill in the mandatory username
+        const string username = "";
+        // Fill in the mandatory password
+        const string password = "";
+        const string baseUrl = "https://accp.tradecloud.nl/api/v1";    
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Tradecloud resend orders.");
+            string[] orderCodes = System.IO.File.ReadAllLines(@"orders.txt");
+
+            HttpClient httpClient = new HttpClient();
+            var base64EncodedUsernamePassword = Convert.ToBase64String(Encoding.ASCII.GetBytes(username + ":" + password));
+            httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", base64EncodedUsernamePassword );
+            
+            foreach (var code in orderCodes)
+            {
+                var id = await getOrderId(code);
+                if (id != null)
+                {
+                    await resendOrder(code, id);
+                }
+            }            
+                        
+            async Task<string> getOrderId(string code)
+            {
+                var getOrderIdUrl = baseUrl + "/purchaseOrder/code/" + code;
+                var response = await httpClient.GetAsync(getOrderIdUrl);
+                var statusCode = (int)response.StatusCode;
+                string id = null;
+                if (statusCode == 200) 
+                {
+                    var result = response.Content.ReadAsStringAsync().Result;
+                    dynamic json = JsonConvert.DeserializeObject<dynamic>(result);
+                    id = json.id;
+                    //Console.WriteLine("getOrderId code=" + code + " id=" +  id + " status=" + statusCode + " reason=" + response.ReasonPhrase);
+                } 
+                else 
+                {
+                    Console.WriteLine("getOrderId code=" + code + " status=" + statusCode + " reason=" + response.ReasonPhrase);
+                } 
+                return id;
+            }
+
+            async Task resendOrder(string code, string id)
+            {                
+                var resendOrderUrl = baseUrl + "/purchaseOrder/" + id + "/_resend";
+                var response = await httpClient.PostAsync(resendOrderUrl, null);
+                var statusCode = (int)response.StatusCode;
+                Console.WriteLine("resendOrder code=" + code + " id=" +  id + " status=" + statusCode + " reason=" + response.ReasonPhrase);
+                // string responseString = await response.Content.ReadAsStringAsync();
+                // if (statusCode == 200)
+                //     Console.WriteLine("ConfirmOrderLine response body=" +  JValue.Parse(responseString).ToString(Formatting.Indented));
+                // else
+                //     Console.WriteLine("ConfirmOrderLine response body=" +  responseString);
+            }
+        }
+    }
+}
