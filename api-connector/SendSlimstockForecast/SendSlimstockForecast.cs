a40509cac125a9e5d6d8ce5253b1a614cc228ba6
Marcel Matthijs <marcel@tradecloud.nl>
2023-02-08 17:21:12 +0100

    [TC-8705] Add custom Slimstock forecast

---
+++ api-connector/SendSlimstockForecast/SendSlimstockForecast.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Com.Tradecloud1.SDK.Client
+{
+    class SendSlimstockForecast
+    {   
+        const bool useToken = true;
+         // https://swagger-ui.accp.tradecloud1.com/?url=https://api.accp.tradecloud1.com/v2/authentication/specs.yaml#/authentication/
+        const string authenticationUrl = "https://api.accp.tradecloud1.com/v2/authentication/";
+        // Fill in mandatory username
+        const string username = "";
+        // Fill in mandatory password
+        const string password = "";
+
+        // https://swagger-ui.accp.tradecloud1.com/?url=https://api.accp.tradecloud1.com/v2/api-connector/specs.yaml#/buyer-endpoints/sendSlimstockForecastByBuyerRoute
+        const string sendForecastUrl = "https://api.accp.tradecloud1.com/v2/api-connector/forecast/slimstock";
+        
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Tradecloud send Slimstock forecast example.");
+
+            var jsonContent = File.ReadAllText(@"slimstock_forecast.json");
+
+            HttpClient httpClient = new HttpClient();
+            if (useToken)
+            {
+                var authenticationClient = new Authentication(httpClient, authenticationUrl);
+                var (accessToken, refreshToken) = await authenticationClient.Login(username, password);
+                httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);            
+            }
+            else
+            {
+                var base64EncodedUsernamePassword = Convert.ToBase64String(Encoding.ASCII.GetBytes(username + ":" + password));
+                httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", base64EncodedUsernamePassword );
+            }
+            await SendSlimstockForecast();
+
+            async Task SendSlimstockForecast()
+            {                
+                var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+                var start = DateTime.Now;
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                var response = await httpClient.PostAsync(sendForecastUrl, content);
+                watch.Stop();
+
+                var statusCode = (int)response.StatusCode;
+                Console.WriteLine("SendForecast start=" + start +  " elapsed=" + watch.ElapsedMilliseconds + "ms status=" + statusCode + " reason=" + response.ReasonPhrase);
+                if (statusCode == 400)
+                     Console.WriteLine("SendForecast request body=" + jsonContent); 
+                string responseString = await response.Content.ReadAsStringAsync();
+                if (statusCode == 200)
+                    Console.WriteLine("SendForecast response body=" +  JValue.Parse(responseString).ToString(Formatting.Indented));
+                else
+                    Console.WriteLine("SendForecast response body=" +  responseString);
+            }
+        }
+    }
+}
