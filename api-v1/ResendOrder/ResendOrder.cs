27ef0e969bb1cf5038b3c77d4089ac11b3df5c1b
Marcel Matthijs <marcel@tradecloud.nl>
2023-02-10 16:20:23 +0100

    Add api v1 ResendOrder

--- api-v1/ResendOrder/ResendOrder.cs
+++ api-v1/ResendOrder/ResendOrder.cs
@@ -8,35 +8,30 @@ using Newtonsoft.Json.Linq;
 
 namespace Com.Tradecloud1.SDK.Client
 {
-    class ResendOrders
+    class ResendOrder
     {   
         // Fill in the mandatory username
         const string username = "";
         // Fill in the mandatory password
         const string password = "";
         const string baseUrl = "https://accp.tradecloud.nl/api/v1";    
+        const string purchaseOrderCode = "";
 
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Tradecloud resend orders.");
-            string[] orderCodes = System.IO.File.ReadAllLines(@"purchase_order_codes.txt");
+            Console.WriteLine("Tradecloud resend order.");
 
             HttpClient httpClient = new HttpClient();
             var base64EncodedUsernamePassword = Convert.ToBase64String(Encoding.ASCII.GetBytes(username + ":" + password));
             httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", base64EncodedUsernamePassword );
             
-            foreach (var code in orderCodes)
-            {
-                var id = await getOrderId(code);
-                if (id != null)
-                {
-                    await resendOrder(code, id);
-                }
-            }            
+            
+            var id = await getOrderId(purchaseOrderCode);
+            await resendOrder(purchaseOrderCode, id);
                         
-            async Task<string> getOrderId(string code)
+            async Task<string> getOrderId(string purchaseOrderCode)
             {
-                var getOrderIdUrl = baseUrl + "/purchaseOrder/code/" + code;
+                var getOrderIdUrl = baseUrl + "/purchaseOrder/code/" + purchaseOrderCode;
                 var response = await httpClient.GetAsync(getOrderIdUrl);
                 var statusCode = (int)response.StatusCode;
                 string id = null;
@@ -49,7 +44,7 @@ namespace Com.Tradecloud1.SDK.Client
                 } 
                 else 
                 {
-                    Console.WriteLine("getOrderId code=" + code + " status=" + statusCode + " reason=" + response.ReasonPhrase);
+                    Console.WriteLine("getOrderId code=" + purchaseOrderCode + " status=" + statusCode + " reason=" + response.ReasonPhrase);
                 } 
                 return id;
             }
@@ -59,7 +54,7 @@ namespace Com.Tradecloud1.SDK.Client
                 var resendOrderUrl = baseUrl + "/purchaseOrder/" + id + "/_resend";
                 var response = await httpClient.PostAsync(resendOrderUrl, null);
                 var statusCode = (int)response.StatusCode;
-                Console.WriteLine("resendOrder code=" + code + " id=" +  id + " status=" + statusCode + " reason=" + response.ReasonPhrase);
+                Console.WriteLine("resendOrder code=" + purchaseOrderCode + " id=" +  id + " status=" + statusCode + " reason=" + response.ReasonPhrase);
                 // string responseString = await response.Content.ReadAsStringAsync();
                 // if (statusCode == 200)
                 //     Console.WriteLine("ConfirmOrderLine response body=" +  JValue.Parse(responseString).ToString(Formatting.Indented));
