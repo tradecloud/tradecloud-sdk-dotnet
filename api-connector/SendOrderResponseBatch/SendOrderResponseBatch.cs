1375a9ccb78dcd34e9701f061913f5c202293c6a
Marcel Matthijs <marcel@tradecloud.nl>
2023-03-31 16:54:38 +0200

    SendOrderResponseBatch WIP

--- api-connector/SendOrderResponseBatch/SendOrderResponseBatch.cs
+++ api-connector/SendOrderResponseBatch/SendOrderResponseBatch.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Globalization;
 using System.IO;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
+using CsvHelper;
+using CsvHelper.Configuration;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
@@ -14,6 +17,7 @@ namespace Com.Tradecloud1.SDK.Client
     {
         const bool dryRun = true;
         const string buyerId = "";
+        const string buyerAccountNumber = "";
         const string accessToken = "";
 
        // https://swagger-ui.accp.tradecloud1.com/?url=https://api.accp.tradecloud1.com/v2/order-line-search/private/specs.yaml#/order-line-search/getByIdRoute
@@ -27,120 +31,124 @@ namespace Com.Tradecloud1.SDK.Client
             Console.WriteLine("Tradecloud order response batch.");
 
             var jsonOrderResponseTemplate = File.ReadAllText(@"order-response-template.json");
-            string[] orderLineConfirmations = System.IO.File.ReadAllLines(@"order-line-confirmations.csv");
-
             HttpClient httpClient = new HttpClient();
             httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
 
             using (var log = new StreamWriter("order-response-batch.log", append: true))
+            using (var reader = new StreamReader("order-line-confirmations.csv"))
+            using (var csvReader = new CsvReader(reader, CultureInfo.InvariantCulture))
             {
-                var count = 0;
-                foreach (var orderLineConfirmation in orderLineConfirmations)
+                
+                csvReader.Context.RegisterClassMap<OrderLineResponseMap>();
+                var orderLineResponses = csvReader.GetRecords<OrderLineResponse>();
+                foreach (var orderLineResponse in orderLineResponses)
                 {
-                    var orderLineConfirmationFields = orderLineConfirmation.Split(",");
-
-                    if (count > 0 && orderLineConfirmationFields.Length == 7)
+                    var lineId = buyerId + "-" + orderLineResponse.PurchaseOrderNumber + "-" + orderLineResponse.PurchaseOrderLinePosition;
+                    var queryResult = await FindOrderLineById(lineId, log);
+                    if (queryResult != null)
                     {
-                        var orderResponse = new OrderResponse 
-                        {
-                            PurchaseOrderNumber = orderLineConfirmationFields[0],
-                            PurchaseOrderLinePosition = orderLineConfirmationFields[1],
-                            DeliveryLinePosition = orderLineConfirmationFields[2],
-                            ConfirmedDate = orderLineConfirmationFields[3],
-                            ConfirmedQuantity = orderLineConfirmationFields[4],
-                            ConfirmedNetPrice = orderLineConfirmationFields[5],
-                            Shipped = orderLineConfirmationFields[6],
-                        };
+                        orderLineResponse.companyId = queryResult["supplierOrder"]["companyId"].ToString();
+                        orderLineResponse.buyerAccountNumber = buyerAccountNumber;
+                        orderLineResponse.currencyIso = queryResult["buyerLine"]["prices"]["netPrice"]["priceInBaseCurrency"]["currencyIso"].ToString();
+                        orderLineResponse.priceUnitOfMeasureIso = queryResult["buyerLine"]["prices"]["priceUnitOfMeasureIso"].ToString();
+                        orderLineResponse.priceUnitQuantity = queryResult["buyerLine"]["prices"]["priceUnitQuantity"].ToString();
 
-                        var lineId = buyerId + "-" + orderResponse.PurchaseOrderNumber + "-" + orderResponse.PurchaseOrderLinePosition;
-                        var queryResult = await FindOrderLineById(lineId, log);
-                        if (queryResult != null)
+                        await log.WriteLineAsync("orderLineResponse=" + JsonConvert.SerializeObject(orderLineResponse));
+                        
+                        if (dryRun)
                         {
-                            string foundLineId = queryResult["id"].ToString();
-                            string foundOrderId = queryResult["orderId"].ToString();
-                            string foundBuyerId = queryResult["buyerOrder"]["companyId"].ToString();
-                            string foundSupplierId = queryResult["supplierOrder"]["companyId"].ToString();
-                            await log.WriteLineAsync("Order line found lineId=" + foundLineId + " orderId=" + foundOrderId + " buyerId=" + foundBuyerId + " supplierId=" + foundSupplierId);
-                            
-                            if (dryRun)
-                            {
-                                await log.WriteLineAsync("SendOrderResponse dry run: lineId=" + foundOrderId);
-                            }
-                            else
-                            {
-                                //await SendOrderResponse(orderResponse, log);
-                            }
+                            await log.WriteLineAsync("SendOrderResponse dry run: orderLineResponse=" + orderLineResponse);
+                        }
+                        else
+                        {
+                            //await SendOrderResponse(orderResponse, log);
                         }
                     }
-                    else if (orderLineConfirmationFields.Length != 7)
-                    {
-                        await log.WriteLineAsync("SendOrderResponse skipped: " + orderLineConfirmation);
-                    }
-
-                    count++;
                 }
+            }
 
-                async Task<JObject> FindOrderLineById(string orderLineId, StreamWriter log)
-                {                
-                    var orderLineSearchUrl = orderLineSearchUrlTemplate.Replace("{orderLineId}", orderLineId);
+            async Task<JObject> FindOrderLineById(string orderLineId, StreamWriter log)
+            {                
+                var orderLineSearchUrl = orderLineSearchUrlTemplate.Replace("{orderLineId}", orderLineId);
 
-                    var start = DateTime.Now;
-                    var watch = System.Diagnostics.Stopwatch.StartNew();
-                    var response = await httpClient.GetAsync(orderLineSearchUrl);
-                    watch.Stop();
+                var start = DateTime.Now;
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                var response = await httpClient.GetAsync(orderLineSearchUrl);
+                watch.Stop();
 
-                    var statusCode = (int)response.StatusCode;
-                    await log.WriteLineAsync("FindOrderLineById orderLineId=" + orderLineId + " start=" + start +  " elapsed=" + watch.ElapsedMilliseconds + "ms status=" + statusCode + " reason=" + response.ReasonPhrase);
-                    string responseString = await response.Content.ReadAsStringAsync();
-                    if (statusCode == 200)
-                    {
-                        await log.WriteLineAsync("FindOrderById response body=" +  JValue.Parse(responseString).ToString(Formatting.Indented));
-                        return JObject.Parse(responseString);
-                    }
-                    else
-                    {
-                        await log.WriteLineAsync("FindOrderById response body=" +  responseString);
-                        return null;
-                    }
+                var statusCode = (int)response.StatusCode;
+                await log.WriteLineAsync("FindOrderLineById orderLineId=" + orderLineId + " start=" + start +  " elapsed=" + watch.ElapsedMilliseconds + "ms status=" + statusCode + " reason=" + response.ReasonPhrase);
+                string responseString = await response.Content.ReadAsStringAsync();
+                if (statusCode == 200)
+                {
+                    //await log.WriteLineAsync("FindOrderById response body=" +  JValue.Parse(responseString).ToString(Formatting.Indented));
+                    return JObject.Parse(responseString);
                 }
-
-                async Task SendOrderResponse(OrderResponse orderResponse, StreamWriter log)
-                {           
-                    var jsonOrderResponse = jsonOrderResponseTemplate;
-                     //   .Replace("{companyId}", orderResponse.
-
-                    var content = new StringContent(jsonOrderResponse, Encoding.UTF8, "application/json");
-
-                    var start = DateTime.Now;
-                    var watch = System.Diagnostics.Stopwatch.StartNew();
-                    var response = await httpClient.PostAsync(sendOrderResponseUrl, content);
-                    watch.Stop();
-
-                    var statusCode = (int)response.StatusCode;
-                    Console.WriteLine("SendOrderResponse start=" + start +  " elapsed=" + watch.ElapsedMilliseconds + "ms status=" + statusCode + " reason=" + response.ReasonPhrase);
-                    if (statusCode == 400)
-                        Console.WriteLine("SendOrderResponse request body=" + jsonOrderResponse); 
-                    string responseString = await response.Content.ReadAsStringAsync();
-                    if (statusCode != 200)
-                        Console.WriteLine("SendOrderResponse response body=" +  responseString);
+                else
+                {
+                    await log.WriteLineAsync("FindOrderById response body=" +  responseString);
+                    return null;
                 }
+            }
+
+            async Task SendOrderResponse(OrderLineResponse orderLineResponse, StreamWriter log)
+            {           
+                var jsonOrderResponse = jsonOrderResponseTemplate
+                    .Replace("{companyId}", orderResponse.companyId)
+                    .Replace("{buyerAccountNumber}", orderResponse.buyerAccountNumber)
+                    .Replace("{purchaseOrderNumber}", orderResponse.purchaseOrderNumber)
+                    .Replace("{purchaseOrderLinePosition}", orderResponse.purchaseOrderLinePosition)
+                    .Replace("{confirmedDeliveryDate}", orderResponse.confirmedDeliveryDate)
+                    .Replace("{confirmedQuantity}", orderResponse.confirmedQuantity)
+                    .Replace("{confirmedNetPrice}", orderResponse.confirmedNetPrice)
+                    .Replace("{currencyIso}", orderResponse.currencyIso)
+                    .Replace("{priceUnitOfMeasureIso}", orderResponse.priceUnitOfMeasureIso)
+                    .Replace("{priceUnitQuantity}", orderResponse.priceUnitQuantity);
+
+                var content = new StringContent(jsonOrderResponse, Encoding.UTF8, "application/json");
+
+                var start = DateTime.Now;
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                var response = await httpClient.PostAsync(sendOrderResponseUrl, content);
+                watch.Stop();
+
+                var statusCode = (int)response.StatusCode;
+                Console.WriteLine("SendOrderResponse start=" + start + " elapsed=" + watch.ElapsedMilliseconds + "ms status=" + statusCode + " reason=" + response.ReasonPhrase);
+                if (statusCode == 400)
+                    Console.WriteLine("SendOrderResponse request body=" + jsonOrderResponse); 
+                string responseString = await response.Content.ReadAsStringAsync();
+                if (statusCode != 200)
+                    Console.WriteLine("SendOrderResponse response body=" +  responseString);
             }
         }
     }
 
-    public class OrderResponse
+    public class OrderLineResponse
     {
-        public string CompanyId { get; set; }
-        public string BuyerAccountNumber { get; set; }
-        public string PurchaseOrderNumber { get; set; }
-        public string PurchaseOrderLinePosition { get; set; } 
-        public string DeliveryLinePosition { get; set; }
-        public string ConfirmedDate { get; set; }
-        public string ConfirmedQuantity { get; set; }
-        public string ConfirmedNetPrice { get; set; }
-        public string CurrencyIso { get; set; }
-        public string PriceUnitOfMeasureIso { get; set; }
-        public string PriceUnitQuantity { get; set; }
-        public string Shipped { get; set; }
+        public string companyId { get; set; }
+        public string buyerAccountNumber { get; set; }
+        public string purchaseOrderNumber { get; set; }
+        public string purchaseOrderLinePosition { get; set; } 
+        public string deliveryLinePosition { get; set; }
+        public string confirmedDeliveryDate { get; set; }
+        public string confirmedQuantity { get; set; }
+        public string confirmedNetPrice { get; set; }
+        public string currencyIso { get; set; }
+        public string priceUnitOfMeasureIso { get; set; }
+        public string priceUnitQuantity { get; set; }
+    }
+
+    public sealed class OrderLineResponseMap : ClassMap<OrderLineResponse>
+    {
+        public OrderLineResponseMap()
+        {
+            AutoMap(CultureInfo.InvariantCulture);
+            Map(m => m.CompanyId).Ignore();
+            Map(m => m.BuyerAccountNumber).Ignore();
+            Map(m => m.deliveryLinePosition).Ignore();
+            Map(m => m.CurrencyIso).Ignore();
+            Map(m => m.PriceUnitOfMeasureIso).Ignore();
+            Map(m => m.PriceUnitQuantity).Ignore();
+        }
     }
 }
