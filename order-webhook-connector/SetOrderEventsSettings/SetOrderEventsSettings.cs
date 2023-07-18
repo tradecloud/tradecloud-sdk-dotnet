cd6453714da79ef77f3b09f703cd3e7858dc5bd5
Marcel Matthijs <marcel@tradecloud.nl>
2023-07-18 11:53:00 +0200

    [TC-9095] Add SetOrderEventsSettings

--- order-webhook-connector/SetOrderEventsSettings/SetOrderEventsSettings.cs
+++ order-webhook-connector/SetOrderEventsSettings/SetOrderEventsSettings.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Com.Tradecloud1.SDK.Client
 {
-    class SetSettings
+    class SetOrderEventsSettings
     {   
         // https://swagger-ui.accp.tradecloud1.com/?url=https://api.accp.tradecloud1.com/v2/authentication/specs.yaml#/authentication/
         const string authenticationUrl = "https://api.accp.tradecloud1.com/v2/authentication/";
@@ -15,22 +15,22 @@ namespace Com.Tradecloud1.SDK.Client
         // Fill in mandatory password
         const string password = "";
 
-        // https://swagger-ui.accp.tradecloud1.com/?url=https://api.accp.tradecloud1.com/v2/sap-soap-connector/private/specs.yaml#/sap-soap-connector/upsertSapSettings
-        const string settingsUrl = "https://api.accp.tradecloud1.com/v2/sap-soap-connector/company/{companyId}/settings";   
+        // https://swagger-ui.accp.tradecloud1.com/?url=https://api.accp.tradecloud1.com/v2/order-webhook-connector/private/specs.yaml#/order-webhook-connector/upsertCompanyOrderEventsIntegrationSettings
+        const string settingsUrl = "https://api.accp.tradecloud1.com/v2/order-webhook-connector/company/f56aa4ce-8ec8-5197-bc26-77716a58add7/settings/orderEvents";   
 
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Tradecloud SAP SOAP Connector set settings example.");
+            Console.WriteLine("Tradecloud Order Webhook Connector set order events settings example.");
 
-            var jsonContent = File.ReadAllText(@"settings.json");
+            var jsonContent = File.ReadAllText(@"order-events-settings.json");
 
             HttpClient httpClient = new HttpClient();
             var authenticationClient = new Authentication(httpClient, authenticationUrl);
             var (accessToken, refreshToken) = await authenticationClient.Login(username, password);
             httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
-            await SetSettings();
+            await SetOrderEventsSettings();
 
-            async Task SetSettings()
+            async Task SetOrderEventsSettings()
             {                
                 var stringContent = new StringContent(jsonContent, Encoding.UTF8, "application/json");
 
@@ -38,10 +38,10 @@ namespace Com.Tradecloud1.SDK.Client
                 var watch = System.Diagnostics.Stopwatch.StartNew();
                 var response = await httpClient.PostAsync(settingsUrl, stringContent);
                 watch.Stop();
-                Console.WriteLine("SetSettings started: " + started +  " elapsed: " + watch.ElapsedMilliseconds);
+                Console.WriteLine("SetOrderEventsSettings started: " + started +  " elapsed: " + watch.ElapsedMilliseconds);
 
                 string responseString = await response.Content.ReadAsStringAsync();
-                Console.WriteLine("SetSettings response StatusCode: " + (int)response.StatusCode + ", body: " +  responseString);  
+                Console.WriteLine("SetOrderEventsSettings response StatusCode: " + (int)response.StatusCode + ", body: " +  responseString);  
             }
         }
     }
