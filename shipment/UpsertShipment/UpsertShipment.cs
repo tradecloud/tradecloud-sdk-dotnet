8d210396ccbf2574a3c5b651a0917ec82a8e857e
Marcel Matthijs <marcel@tradecloud.nl>
2023-08-28 14:53:56 +0200

    [TC-9051] Rename to UpsertShipment

---
+++ shipment/UpsertShipment/UpsertShipment.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Com.Tradecloud1.SDK.Client
+{
+    class UpsertShipment
+    {   
+        const bool useToken = true;
+         // https://swagger-ui.accp.tradecloud1.com/?url=https://api.accp.tradecloud1.com/v2/authentication/specs.yaml#/authentication/
+        const string authenticationUrl = "https://api.accp.tradecloud1.com/v2/authentication/";
+
+        // Fill in mandatory username
+        const string username = "";
+        // Fill in mandatory password
+        const string password = "";
+
+        // https://swagger-ui.test.tradecloud1.com/?url=https://api.test.tradecloud1.com/v2/shipment/private/specs.yaml#/shipment/createShipmentBySupplier
+        const string upsertShipmentUrl = "https://api.accp.tradecloud1.com/v2/shipment/despatch-advice";
+        
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Tradecloud upsert shipment based on despatch advice by supplier example.");
+
+            var jsonContent = File.ReadAllText(@"despatch-advice.json");
+
+            HttpClient httpClient = new HttpClient();
+            if (useToken)
+            {
+                var authenticationClient = new Authentication(httpClient, authenticationUrl);
+                var (accessToken, refreshToken) = await authenticationClient.Login(username, password);
+                httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);            
+            }
+            else
+            {
+                var base64EncodedUsernamePassword = Convert.ToBase64String(Encoding.ASCII.GetBytes(username + ":" + password));
+                httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", base64EncodedUsernamePassword );
+            }
+            await UpsertShipment();
+
+            async Task UpsertShipment()
+            {                
+                var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+                var start = DateTime.Now;
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                var response = await httpClient.PostAsync(upsertShipmentUrl, content);
+                watch.Stop();
+
+                var statusCode = (int)response.StatusCode;
+                Console.WriteLine("UpsertShipment start=" + start +  " elapsed=" + watch.ElapsedMilliseconds + "ms status=" + statusCode + " reason=" + response.ReasonPhrase);
+                if (statusCode == 400)
+                     Console.WriteLine("UpsertShipment request body=" + jsonContent); 
+                string responseString = await response.Content.ReadAsStringAsync();
+                if (statusCode == 200)
+                    Console.WriteLine("UpsertShipment response body=" +  JValue.Parse(responseString).ToString(Formatting.Indented));
+                else
+                    Console.WriteLine("UpsertShipment response body=" +  responseString);
+            }
+        }
+    }
+}
