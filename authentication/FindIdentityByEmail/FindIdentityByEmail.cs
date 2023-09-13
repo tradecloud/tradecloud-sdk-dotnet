f8fd059dcaccc6e59cc2f3759ef5a7a62d34408e
Marcel Matthijs <marcel@tradecloud.nl>
2023-09-13 16:24:50 +0200

    Improve FindIdentity/User

--- authentication/FindIdentityByEmail/FindIdentityByEmail.cs
+++ authentication/FindIdentityByEmail/FindIdentityByEmail.cs
@@ -10,18 +10,18 @@ namespace Com.Tradecloud1.SDK.Client
     class FindIdentityByEmail
     {   
         // https://swagger-ui.accp.tradecloud1.com/?url=https://api.accp.tradecloud1.com/v2/authentication/specs.yaml#/authentication/login
-        const string authenticationUrl = "https://api.tradecloud1.com/v2/authentication/";
+        const string authenticationUrl = "https://api.accp.tradecloud1.com/v2/authentication/";
         // Fill in mandatory username
        const string username = "";
         // Fill in mandatory password
         const string password = "";
   
         // https://swagger-ui.accp.tradecloud1.com/?url=https://api.accp.tradecloud1.com/v2/authentication/internal/specs.yaml#/authentication/findIdentityByEmail
-        const string getUserByEmailUrl = "https://api.tradecloud1.com/v2/authentication/find";
+        const string getUserByEmailUrl = "https://api.accp.tradecloud1.com/v2/authentication/find";
 
         const string jsonContentWithSingleQuotes = 
             @"{
-               `email`: `commercial@comotech.fr`
+               `email`: ``
             }";
 
         static async Task Main(string[] args)
@@ -40,13 +40,19 @@ namespace Com.Tradecloud1.SDK.Client
                 var jsonContent = jsonContentWithSingleQuotes.Replace("`", "\"");
                 var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
 
+                var start = DateTime.Now;
                 var watch = System.Diagnostics.Stopwatch.StartNew();
                 var response = await httpClient.PostAsync(getUserByEmailUrl, content);
                 watch.Stop();
-                Console.WriteLine("FindIdentityByEmail StatusCode: " + (int)response.StatusCode + " ElapsedMilliseconds: " + watch.ElapsedMilliseconds);
+
+                var statusCode = (int)response.StatusCode;
+                Console.WriteLine("FindIdentityByEmail start=" + start +  " elapsed=" + watch.ElapsedMilliseconds + "ms status=" + statusCode + " reason=" + response.ReasonPhrase);
 
                 string responseString = await response.Content.ReadAsStringAsync();
-                Console.WriteLine("FindIdentityByEmail Body: " +  JValue.Parse(responseString).ToString(Formatting.Indented)); 
+                if (statusCode == 200)
+                    Console.WriteLine("FindIdentityByEmail response body=" +  JValue.Parse(responseString).ToString(Formatting.Indented));
+                else
+                    Console.WriteLine("FindIdentityByEmail response body=" +  responseString);
             }
         }
     }
