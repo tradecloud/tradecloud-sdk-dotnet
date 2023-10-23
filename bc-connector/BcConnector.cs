c45fd009ed3573115dffe96f212a149487dc056d
Marcel Matthijs <marcel@tradecloud.nl>
2023-10-23 20:47:19 +0200

    BC Connector WIP

---
+++ bc-connector/BcConnector.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Com.Tradecloud1.SDK.Client
+{
+    class BcConnector
+    {           
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tradecloud Business Central Connector.");
+        }
+    }
+}
