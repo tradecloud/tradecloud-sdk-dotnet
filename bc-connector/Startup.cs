a12a0ab503a8b356ee776229658dec411cc95b81
Marcel Matthijs <marcel@tradecloud.nl>
2023-10-26 14:04:23 +0200

    Upgrade to .NET 7.0

--- bc-connector/Startup.cs
+++ bc-connector/Startup.cs
@@ -11,6 +11,11 @@ public class Startup
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
         app.UseRouting();
 
         app.UseEndpoints(endpoints =>
