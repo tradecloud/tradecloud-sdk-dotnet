ad181b96f193a9827fcbb24e7ce24b07b9431106
Marcel Matthijs <marcel@tradecloud.nl>
2023-12-04 13:52:54 +0100

    Improve FindOrderTasks

--- workflow/FindOrderTasks/FindOrderTasks.cs
+++ workflow/FindOrderTasks/FindOrderTasks.cs
@@ -1,11 +1,9 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Text;
 using System.Net.Http;
 using System.Threading.Tasks;
 
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace Com.Tradecloud1.SDK.Client
@@ -13,8 +11,8 @@ namespace Com.Tradecloud1.SDK.Client
     class FindOrderTasks
     {   
         const string accessToken = "";
-        const string assigneeCompanyId = "{companyId}"; // Tasks for this buyer or supplier.
-        const string orderIdToFind = "{buyerId}-{purchaseOrderNumber}"; // Find all tasks related to this `orderId` for above buyer or supplier.
+        const string assigneeCompanyId = "{companyId}"; // Tasks assigned to this buyer or supplier.
+        const string assigneeContactId = "{contactId}"; // Tasks assigned to this user of above buyer or supplier.
 
         // https://swagger-ui.accp.tradecloud1.com/?url=https://api.accp.tradecloud1.com/v2/workflow/private/specs.yaml#/workflow/searchWorkflowTasksRoute
         const string workflowSearchUrl = "https://api.accp.tradecloud1.com/v2/workflow/search";
@@ -24,7 +22,7 @@ namespace Com.Tradecloud1.SDK.Client
             'filters': {
                 'assignedTo': 'MyCompany',
                 'companyId': '{companyId}',
-                'taskStatus': 'created' 
+                'contactUserIds': ['{contactId}']
             },
             'offset': {offset},
             'limit': {limit}
@@ -62,7 +60,7 @@ namespace Com.Tradecloud1.SDK.Client
             async Task<JObject> SearchWorkflow(int offset)
             {
                 var queryTemplate = queryTemplateWithSingleQuotes.Replace("'", "\"");
-                var query = queryTemplate.Replace("{companyId}", assigneeCompanyId).Replace("{offset}", offset.ToString()).Replace("{limit}", limit.ToString());
+                var query = queryTemplate.Replace("{companyId}", assigneeCompanyId).Replace("{contactId}", assigneeContactId).Replace("{offset}", offset.ToString()).Replace("{limit}", limit.ToString());
                 var content = new StringContent(query, Encoding.UTF8, "application/json");
 
                 var start = DateTime.Now;
