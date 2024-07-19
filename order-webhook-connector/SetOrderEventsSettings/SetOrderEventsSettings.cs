37858167952804b50f6ceeba69a440c1feec3891
Marcel Matthijs <marcel@tradecloud.nl>
2024-07-19 17:51:16 +0200

    [TC-9860] add RevertCancelledOrderLine

--- order-webhook-connector/SetOrderEventsSettings/SetOrderEventsSettings.cs
+++ order-webhook-connector/SetOrderEventsSettings/SetOrderEventsSettings.cs
@@ -7,41 +7,37 @@ using System.Threading.Tasks;
 namespace Com.Tradecloud1.SDK.Client
 {
     class SetOrderEventsSettings
-    {   
-        // https://swagger-ui.accp.tradecloud1.com/?url=https://api.accp.tradecloud1.com/v2/authentication/specs.yaml#/authentication/
-        const string authenticationUrl = "https://api.accp.tradecloud1.com/v2/authentication/";
-
-        const string username = "";
-        // Fill in mandatory password
-        const string password = "";
+    {
+        const string accessToken = ";
+        const string companyId = "";
+        const string body = "order-events-settings.json";
 
         // https://swagger-ui.accp.tradecloud1.com/?url=https://api.accp.tradecloud1.com/v2/order-webhook-connector/private/specs.yaml#/order-webhook-connector/upsertCompanyOrderEventsIntegrationSettings
-        const string settingsUrl = "https://api.accp.tradecloud1.com/v2/order-webhook-connector/company/f56aa4ce-8ec8-5197-bc26-77716a58add7/settings/orderEvents";   
+        const string settingsUrlTemplate = "https:/api.accp.tradecloud1.com/v2/order-webhook-connector/company/{companyId}/settings/orderEvents";
 
         static async Task Main(string[] args)
         {
             Console.WriteLine("Tradecloud Order Webhook Connector set order events settings example.");
 
-            var jsonContent = File.ReadAllText(@"order-events-settings.json");
+            var settingsUrl = settingsUrlTemplate.Replace("{companyId}", companyId);
+            var jsonContent = File.ReadAllText(@body);
 
             HttpClient httpClient = new HttpClient();
-            var authenticationClient = new Authentication(httpClient, authenticationUrl);
-            var (accessToken, refreshToken) = await authenticationClient.Login(username, password);
             httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
             await SetOrderEventsSettings();
 
             async Task SetOrderEventsSettings()
-            {                
+            {
                 var stringContent = new StringContent(jsonContent, Encoding.UTF8, "application/json");
 
                 var started = DateTime.Now;
                 var watch = System.Diagnostics.Stopwatch.StartNew();
                 var response = await httpClient.PostAsync(settingsUrl, stringContent);
                 watch.Stop();
-                Console.WriteLine("SetOrderEventsSettings started: " + started +  " elapsed: " + watch.ElapsedMilliseconds);
+                Console.WriteLine("SetOrderEventsSettings started: " + started + " elapsed: " + watch.ElapsedMilliseconds);
 
                 string responseString = await response.Content.ReadAsStringAsync();
-                Console.WriteLine("SetOrderEventsSettings response StatusCode: " + (int)response.StatusCode + ", body: " +  responseString);  
+                Console.WriteLine("SetOrderEventsSettings response StatusCode: " + (int)response.StatusCode + ", body: " + responseString);
             }
         }
     }
---
+++ order/RevertCancelledOrderLine/RevertCancelledOrderLine.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Com.Tradecloud1.SDK.Client
+{
+    class RevertCancelledOrderLine
+    {
+        const string accessToken = "";
+        const string orderId = "";
+        const string body = "revert.json";
+
+        // https://swagger-ui.accp.tradecloud1.com/?url=https://api.accp.tradecloud1.com/v2/order/private/specs.yaml#/order/revertCancelledOrderLines
+        const string revertUrlTemplate = "https://api.accp.tradecloud1.com/v2/order/{orderId}/revertCancelled";
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Tradecloud revert cancelled order line example.");
+
+            var jsonContent = File.ReadAllText(@body);
+
+            HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
+            await RevertCancelledOrderLine();
+
+            async Task RevertCancelledOrderLine()
+            {
+                var revertUrl = HttpUtility.UrlPathEncode(revertUrlTemplate.Replace("{orderId}", orderId));
+                var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+                var start = DateTime.Now;
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                var response = await httpClient.PostAsync(revertUrl, content);
+                watch.Stop();
+
+                var statusCode = (int)response.StatusCode;
+                Console.WriteLine("RevertCancelledOrderLine start=" + start + " elapsed=" + watch.ElapsedMilliseconds + "ms status=" + statusCode + " reason=" + response.ReasonPhrase);
+                if (statusCode == 400)
+                    Console.WriteLine("RevertCancelledOrderLine request body=" + jsonContent);
+                string responseString = await response.Content.ReadAsStringAsync();
+                if (statusCode == 200)
+                    Console.WriteLine("RevertCancelledOrderLine response body=" + JValue.Parse(responseString).ToString(Formatting.Indented));
+                else
+                    Console.WriteLine("RevertCancelledOrderLine response body=" + responseString);
+            }
+        }
+    }
+}
