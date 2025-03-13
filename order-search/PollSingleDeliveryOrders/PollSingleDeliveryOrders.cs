b5cec219f1c90f5362bfb2c5d640e4557fc9fefd
Marcel Matthijs <marcel.matthijs@tradecloud1.com>
2025-03-13 14:06:04 +0100

    add poll single delivery orders

--- order-search/PollSingleDeliveryOrders/PollSingleDeliveryOrders.cs
+++ order-search/PollSingleDeliveryOrders/PollSingleDeliveryOrders.cs
@@ -7,24 +7,25 @@ using Newtonsoft.Json.Linq;
 
 namespace Com.Tradecloud1.SDK.Client
 {
-    class PollOrders
+    class PollSingleDeliveryOrders
     {
         const bool useToken = true;
         // https://swagger-ui.accp.tradecloud1.com/?url=https://api.accp.tradecloud1.com/v2/authentication/specs.yaml#/authentication/login
         const string authenticationUrl = "https://api.accp.tradecloud1.com/v2/authentication/";
+
         // Fill in mandatory username
         const string username = "";
         // Fill in mandatory password
         const string password = "";
 
-        // https://swagger-ui.accp.tradecloud1.com/?url=https://api.accp.tradecloud1.com/v2/order-search/specs.yaml#/order-search/pollOrdersRoute
-        const string orderSearchUrl = "https://api.accp.tradecloud1.com/v2/order-search/poll";
+        // https://swagger-ui.accp.tradecloud1.com/?url=https://api.accp.tradecloud1.com/v2/order-search/specs.yaml#/order-search/pollOrdersSingleDeliveryRoute
+        const string orderSearchUrl = "https://api.accp.tradecloud1.com/v2/order-search/poll/single-delivery";
 
         // Fill in the search query
         const string jsonContentWithSingleQuotes = @"{
             'filters': {
                 'companyId': 'f56aa4ce-8ec8-5197-bc26-77716a58add7',
-                'lastUpdatedAfter': '2025-03-13T12:45:02.110Z'
+                'lastUpdatedAfter': '2025-03-13T12:48:02.110Z'
             },
             'offset': 0,
             'limit': 100
@@ -32,7 +33,7 @@ namespace Com.Tradecloud1.SDK.Client
 
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Tradecloud poll orders example.");
+            Console.WriteLine("Tradecloud poll single delivery orders example.");
 
             HttpClient httpClient = new HttpClient();
             if (useToken)
@@ -46,9 +47,9 @@ namespace Com.Tradecloud1.SDK.Client
                 var base64EncodedUsernamePassword = Convert.ToBase64String(Encoding.ASCII.GetBytes(username + ":" + password));
                 httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", base64EncodedUsernamePassword);
             }
-            await PollOrders();
+            await PollSingleDeliveryOrders();
 
-            async Task PollOrders()
+            async Task PollSingleDeliveryOrders()
             {
                 var jsonContent = jsonContentWithSingleQuotes.Replace("'", "\"");
                 var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
@@ -59,14 +60,14 @@ namespace Com.Tradecloud1.SDK.Client
                 watch.Stop();
 
                 var statusCode = (int)response.StatusCode;
-                Console.WriteLine("PollOrders start=" + start + " elapsed=" + watch.ElapsedMilliseconds + "ms status=" + statusCode + " reason=" + response.ReasonPhrase);
+                Console.WriteLine("PollSingleDeliveryOrders start=" + start + " elapsed=" + watch.ElapsedMilliseconds + "ms status=" + statusCode + " reason=" + response.ReasonPhrase);
                 if (statusCode == 400)
-                    Console.WriteLine("PollOrders request body=" + jsonContent);
+                    Console.WriteLine("PollSingleDeliveryOrders request body=" + jsonContent);
                 string responseString = await response.Content.ReadAsStringAsync();
                 if (statusCode == 200)
-                    Console.WriteLine("PollOrders response body=" + JValue.Parse(responseString).ToString(Formatting.Indented));
+                    Console.WriteLine("PollSingleDeliveryOrders response body=" + JValue.Parse(responseString).ToString(Formatting.Indented));
                 else
-                    Console.WriteLine("PollOrders response body=" + responseString);
+                    Console.WriteLine("PollSingleDeliveryOrders response body=" + responseString);
             }
         }
     }
