1ba1dedc0c2a2bf89f3b6b8c064a7c7684978ada
Marcel Matthijs <marcel@tradecloud.nl>
2025-04-10 16:35:24 +0200

    Improve send batch orders

--- api-connector/SendOrderBatch/SendOrderBatch.cs
+++ api-connector/SendOrderBatch/SendOrderBatch.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.IO;
 using System.Text;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Linq;
 
@@ -8,181 +10,53 @@ namespace Com.Tradecloud1.SDK.Client
 {
     class SendOrderBatch
     {   
-         // https://swagger-ui.accp.tradecloud1.com/?url=https://api.accp.tradecloud1.com/v2/authentication/specs.yaml#/authentication/
-        const string authenticationUrl = "https://api.accp.tradecloud1.com/v2/authentication/";
-        // Fill in mandatory username
-        const string username = "";
-        // Fill in mandatory password
-        const string password = "";
+        const string accessToken = "";
 
         // https://swagger-ui.accp.tradecloud1.com/?url=https://api.accp.tradecloud1.com/v2/api-connector/specs.yaml#/buyer-endpoints/sendOrderByBuyerRoute
-        const string sendOrderUrl = "https://api.accp.tradecloud1.com/v2/api-connector/order";
-
-        // Check/amend manadatory order
-        const string jsonContentTemplateWithSingleQuotes = @"{
-            `order`: {
-                `companyId`: `f56aa4ce-8ec8-5197-bc26-77716a58add7`,
-                `supplierAccountNumber`: `540830`,
-                `purchaseOrderNumber`: `<purchaseOrderNumber>`,
-                `description`: `Any buyer custom text about this order`,
-                `destination`: {
-                    `code`: `001`,
-                    `names`: [
-                        `My Company Warehouse`,
-                        `Dock 12`
-                    ],
-                    `addressLines`: [
-                        `Street 123`,
-                        `Area 52`
-                    ],
-                    `postalCode`: `1234 AB`,
-                    `city`: `Rotterdam`,
-                    `countryCodeIso2`: `NL`,
-                    `countryName`: `the Netherlands`,
-                    `locationType`: `warehouse`
-                },
-                `terms`: {
-                    `incotermsCode`: `CIF`,
-                    `incoterms`: `ECT Rotterdam`,
-                    `paymentTermsCode`: `30D`,
-                    `paymentTerms`: `30 days`
-                },
-                `indicators`: {
-                    `noDeliveryExpected`: false,
-                    `shipped`: false,
-                    `delivered`: false,
-                    `completed`: false
-                },
-                `properties`: [
-                    {
-                        `key`: `color`,
-                        `value`: `red`
-                    }
-                ],
-                `notes`: [
-                    `one order note`,
-                    `another order note`
-                ],
-                `labels`: [
-                    `one order label`,
-                    `another order label`,
-                    `third order label`
-                ],
-                `contact`: {
-                    `email`: `frankjan@tradecloud1.com`
-                }
-            },
-            `lines`: [
-                {
-                    `position`: `0001`,
-                    `row`: `1`,
-                    `description`: `Any buyer text about this line`,
-                    `item`: {
-                        `number`: `12345`,
-                        `revision`: `v2`,
-                        `name`: `Round tube ø60x45`,
-                        `description`: `Very nice round tube ø60 x 45`,
-                        `purchaseUnitOfMeasureIso`: `PCE`,
-                        `supplierItemNumber`: `67890`
-                    },
-                    `itemDetails`: {
-                        `countryOfOriginCodeIso2`: `NL`,
-                        `combinedNomenclatureCode`: `6406 10 10`,
-                        `netWeight`: 41.241,
-                        `netWeightUnitOfMeasureIso`: `KG`,
-                        `dangerousGoodsCodeUnece`: `0060`,
-                        `serialNumber`: `0x0000000000028000`,
-                        `batchNumber`: `#18001`
-                    },
-                    `deliverySchedule`: [
-                          {
-                            `position`: `0001`,
-                            `date`: `2021-11-01`,
-                            `quantity`: 1234.56
-                        }
-                    ], 
-                    `prices`: {
-                        `grossPrice`: {
-                            `priceInTransactionCurrency`: {
-                                `value`: 1234.56,
-                                `currencyIso`: `EUR`
-                            },
-                            `priceInBaseCurrency`: {
-                                `value`: 1234.56,
-                                `currencyIso`: `EUR`
-                            }
-                        },
-                        `discountPercentage`: 50,
-                        `netPrice`: {
-                            `priceInTransactionCurrency`: {
-                                `value`: 1234.56,
-                                `currencyIso`: `EUR`
-                            },
-                            `priceInBaseCurrency`: {
-                                `value`: 1234.56,
-                                `currencyIso`: `EUR`
-                            }
-                        },
-                        `priceUnitOfMeasureIso`: `PCE`,
-                        `priceUnitQuantity`: 100
-                    },                 
-                    `terms`: {
-                        `contractNumber`: `123456789`,
-                        `contractPosition`: `0001`
-                    },
-                    `projectNumber`: `PROJ12345`,
-                    `productionNumber`: `PROD12345`,
-                    `salesOrderNumber`: `SO123456789`,
-                    `indicators`: {
-                        `noDeliveryExpected`: false,
-                        `shipped`: false,
-                        `delivered`: false,
-                        `completed`: false
-                    },
-                    `properties`: [
-                        {
-                        `key`: `color`,
-                        `value`: `red`
-                        }
-                    ],
-                    `notes`: [
-                        `one line note`,
-                        `another line note`
-                    ],
-                    `labels`: [
-                        `one line label`,
-                        `another line label`
-                    ]
-                }
-            ],
-            `erpIssueDateTime`: `2019-12-31T10:11:12`,
-            `erpIssuedBy`: {
-                `email`: `frankjan@tradecloud1.com`
-            },
-            `erpLastChangeDateTime`: `2019-12-31T10:11:12`,
-            `erpLastChangedBy`: {
-                `email`: `contact@yourcompany.com`
-            }
-        }";
+        const string sendOrderUrl = "https://api.test.tradecloud1.com/v2/api-connector/order";
+    
+        // https://swagger-ui.accp.tradecloud1.com/?url=https://api.accp.tradecloud1.com/v2/api-connector/specs.yaml#/buyer-endpoints/sendOrderIndicatorsByBuyerRoute
+        const string sendOrderIndicatorsUrl = "https://api.test.tradecloud1.com/v2/api-connector/order/indicators";
 
         static async Task Main(string[] args)
         {
             Console.WriteLine("Tradecloud send order batch.");
-            var jsonContentTemplate = jsonContentTemplateWithSingleQuotes.Replace("`", "\"");        
+            var orderJsonContent = File.ReadAllText("minimal-order.json");
+            var orderIndicatorsJsonContent = File.ReadAllText("order-line-indicators-cancelled.json");
+
             var random = new Random();
              // 500 is the max. parallism for a single API connector instance in a test environment
-            var purchaseOrderNumbers = Enumerable.Range(1,500).Select(r => random.Next(1000, 1000000000).ToString("0000000000"));
+            var purchaseOrderNumbers = Enumerable.Range(1,10000).Select(r => random.Next(1000, 1000000000).ToString("0000000000")).ToList();
+            Console.WriteLine("Issueing and canceling batch orders, count: " + purchaseOrderNumbers.Count() + 
+                ", first 5: " + string.Join(", ", purchaseOrderNumbers.Take(5)) + 
+                ", last 5: " + string.Join(", ", purchaseOrderNumbers.TakeLast(5)));
 
             HttpClient httpClient = new HttpClient();
-            var authenticationClient = new Authentication(httpClient, authenticationUrl);
-            var (accessToken, refreshToken) = await authenticationClient.Login(username, password);
             httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
             
             try {                
                 var watch = System.Diagnostics.Stopwatch.StartNew();
-                await Task.WhenAll(purchaseOrderNumbers.Select(SendOrder));
+                // await Task.WhenAll(purchaseOrderNumbers.Select(SendOrder)); - parallel execution
+                foreach (var poNumber in purchaseOrderNumbers)
+                {
+                    await SendOrder(poNumber); // waits for each one before starting the next
+                }
                 watch.Stop();
-                Console.WriteLine("SendOrderBatch done, Count: " + purchaseOrderNumbers.Count() + " ElapsedMilliseconds: " + watch.ElapsedMilliseconds);
+                Console.WriteLine("Serial sending orders done, took: " + watch.ElapsedMilliseconds + " ms");
+
+                // Await order service processing before cancelling as the API connector checks if the order exists
+                var awaitTime = purchaseOrderNumbers.Count() * 10;         
+                Console.WriteLine("Awaiting order service processing before cancelling... " + awaitTime + " ms");
+                Thread.Sleep(awaitTime);        
+
+                watch = System.Diagnostics.Stopwatch.StartNew();
+                // await Task.WhenAll(purchaseOrderNumbers.Select(SendOrderIndicators)); - parallel execution
+                foreach (var poNumber in purchaseOrderNumbers)
+                {
+                    await SendOrderIndicators(poNumber); // waits for each one before starting the next
+                }
+                watch.Stop();
+                Console.WriteLine("Serial sending order indicators, took: " + watch.ElapsedMilliseconds + " ms");
             }
             catch (Exception ex) {
                 httpClient.CancelPendingRequests();
@@ -191,9 +65,36 @@ namespace Com.Tradecloud1.SDK.Client
 
            async Task SendOrder(string purchaseOrderNumber)
            {
-                var jsonContent = jsonContentTemplate.Replace("<purchaseOrderNumber>", purchaseOrderNumber);
+                var jsonContent = orderJsonContent.Replace("{purchaseOrderNumber}", purchaseOrderNumber);
                 var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
-                await httpClient.PostAsync(sendOrderUrl, content);
+                //Console.WriteLine("Sending order: " + jsonContent);
+                var response = await httpClient.PostAsync(sendOrderUrl, content);
+
+                var statusCode = (int)response.StatusCode;
+                if (statusCode == 400) {
+                     Console.WriteLine("SendOrder status=" + statusCode + " request body=" + jsonContent); 
+                }                
+                if (statusCode != 200) {
+                    string responseString = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine("SendOrder status=" + statusCode + " response body=" +  responseString);
+                }
+           }
+
+           async Task SendOrderIndicators(string purchaseOrderNumber)
+           {
+                var jsonContent = orderIndicatorsJsonContent.Replace("{purchaseOrderNumber}", purchaseOrderNumber);
+                var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+                //Console.WriteLine("Sending order indicators: " + jsonContent);
+                var response = await httpClient.PostAsync(sendOrderIndicatorsUrl, content);
+
+                var statusCode = (int)response.StatusCode;
+                if (statusCode == 400) {
+                     Console.WriteLine("SendOrderIndicators status=" + statusCode + " request body=" + jsonContent); 
+                }                
+                if (statusCode != 200) {
+                    string responseString = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine("SendOrderIndicators status=" + statusCode + " response body=" +  responseString);
+                }
            }
         }
     }
