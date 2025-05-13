4f39d9c60156e0fd0897711b94801b9bafaba692
Marcel Matthijs <marcel@tradecloud.nl>
2025-05-13 20:49:35 +0200

    Add shipment webhook settings

---
+++ shipment-webhook-connector/GetSettings/GetSettings.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Com.Tradecloud1.SDK.Client
+{
+    class GetSettings
+    {
+        const string accessToken = "";
+        const string companyId = "";
+
+        // https://swagger-ui.accp.tradecloud1.com/?url=https://api.accp.tradecloud1.com/v2/shipment-webhook-connector/private/specs.yaml#/shipment-webhook-connector/getCompanyIntegrationSettings
+        const string settingsUrlTemplate = "https://api.accp.tradecloud1.com/v2/shipment-webhook-connector/company/{companyId}/settings";
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Tradecloud Shipment Webhook Connector get settings example.");
+            HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
+            await GetSettings();
+
+            async Task GetSettings()
+            {
+                var settingsUrl = settingsUrlTemplate.Replace("{companyId}", companyId);
+
+                var start = DateTime.Now;
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                var response = await httpClient.GetAsync(settingsUrl);
+                watch.Stop();
+
+                var statusCode = (int)response.StatusCode;
+                Console.WriteLine("GetSettings start=" + start + " elapsed=" + watch.ElapsedMilliseconds + "ms status=" + statusCode + " reason=" + response.ReasonPhrase);
+                string responseString = await response.Content.ReadAsStringAsync();
+                if (statusCode == 200)
+                    Console.WriteLine("GetSettings response body=" + JValue.Parse(responseString).ToString(Formatting.Indented));
+                else
+                    Console.WriteLine("GetSettings response body=" + responseString);
+            }
+        }
+    }
+}
