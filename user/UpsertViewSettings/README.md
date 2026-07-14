079962b0325850d9871e45b2f45bab8c8217d6f5
Marcel Matthijs <marcel.matthijs@tradecloud1.com>
2026-07-14 21:44:32 +0200

    Add UpsertViewSettings

---
+++ user/UpsertViewSettings/README.md
@@ -0,0 +1,19 @@
+# Update User View Settings
+
+This example upserts the user view settings
+
+## Configure
+
+In the source code:
+
+- amend authenticationUrl
+- fill in username
+- fill in password
+- add <userId> in upsertViewSettingsUrl
+- amend view-settings.json
+
+## Run
+
+```
+➜  
+```
---
+++ user/UpsertViewSettings/UpsertViewSettings.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Com.Tradecloud1.SDK.Client
+{
+    class UpsertViewSettings
+    {
+        // https://swagger-ui.accp.tradecloud1.com/?url=https://api.accp.tradecloud1.com/v2/authentication/specs.yaml#/authentication/
+        const string authenticationUrl = "https://api.accp.tradecloud1.com/v2/authentication/";
+        // Fill in mandatory username
+        const string username = "";
+        // Fill in mandatory password
+        const string password = "";
+        // Add user add url
+        const string upsertViewSettingsUrl = "https://api.accp.tradecloud1.com/v2/user/view-settings";
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Tradecloud upsert view settings example.");
+
+            var jsonContent = File.ReadAllText(@"view-settings.json");
+
+            HttpClient httpClient = new HttpClient();
+            var authenticationClient = new Authentication(httpClient, authenticationUrl);
+            var (accessToken, refreshToken) = await authenticationClient.Login(username, password);
+            await UpsertViewSettings(accessToken);
+
+            async Task UpsertViewSettings(string accessToken)
+            {
+                httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
+                var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                var response = await httpClient.PostAsync(upsertViewSettingsUrl, content);
+                watch.Stop();
+                Console.WriteLine("UpsertViewSettings StatusCode: " + (int)response.StatusCode + " ElapsedMilliseconds: " + watch.ElapsedMilliseconds);
+
+                string responseString = await response.Content.ReadAsStringAsync();
+                Console.WriteLine("UpsertViewSettings Body: " + responseString);
+            }
+        }
+    }
+}
